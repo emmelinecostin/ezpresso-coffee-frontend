178b71fab4df4e9b99620b203b708a1fa1186488
Emmeline Costin <emmecostin@gmail.com>
2021-09-30 22:12:02 -0700

    added menu items in html

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,11 +2,44 @@ import Ezpresso from '../Images/Ezpresso.jpg'
 
 const Menu = () => {
 
-    return ( 
+    return (
         <div>
-            <img className="menuImg" src={Ezpresso} alt="ezpresso" />
+            <div className="menuImg">
+                <img src={Ezpresso} alt="ezpresso" />
+            </div>
+            <div className="menuHeading">
+                <h1>Ezpresso Menu</h1>
+            </div>
+            <div className="menuContainer">
+                <div className="coffeContainer">
+                    <p>Hot Coffee</p>
+                    <p>Iced Coffee</p>
+                    <p>Americano</p>
+                </div>
+                <div className="milkContainer">
+                    <p>Whole Milk</p>
+                    <p>Half & Half</p>
+                    <p>Almond Milk</p>
+                    <p>Oat Milk</p>
+                </div>
+                <div className="flavorsContainer">
+                    <p>Vanilla</p>
+                    <p>Caramel</p>
+                    <p>Hazelnut</p>
+                    <p>Dark Chocolate</p>
+                    <p>White Chocolate</p>
+                    <p>Local Honey</p>
+                </div>
+            </div>
+            <div className="request">
+                <div>
+                    We are more than happy to add in your favorite flavor as a special. 
+                    We can also add italian soda in your package as well, we kindly ask that you specify 
+                    your needs and desires for your event! 
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Menu;
